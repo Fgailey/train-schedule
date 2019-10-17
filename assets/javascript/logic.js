777b03935e6255da44e0e1e47c15d7600af872fb
Foster Gailey <foster@gaileyhome.com>
2019-10-17 17:47:02 -0400

    added moment stuff

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -28,9 +28,68 @@
             destination: destination,
             firstTime: firstTime,
             frequency: frequency,
-            currentTime: firebase.database.ServerValue.TIMESTAMP
+            currentTime: firebase.database.ServerValue.TIMESTAMP,
         });
-        console.log("yes")
 
+        // let keyRef = db.ref();
+        // keyRef.once("value")
+        // .then(function(keySnap) {
+        //     var key = keySnap.key;
+        //     console.log(key)
+        // });
+        // console.log("yes")
+        
+        $("#train-name").val("");
+        $("#destination").val("");
+        $("#first-train").val("");
+        $("#frequency").val("");
         
     })
+    db.ref().on("child_added", function(snapshot) {
+        let sv = snapshot.val();
+        
+        // let key = sv.key
+        // console.log(key)
+
+        let tr = $("<tr>");
+        tr.addClass("trainRow")
+        $("#train-schedule").append(tr)
+
+        let tdn = $("<td>");
+        tdn.text(sv.trainName);
+        $(tr).append(tdn);
+
+        let tdd = $("<td>");
+        tdd.text(sv.destination);
+        $(tr).append(tdd);
+
+        let tdf = $("<td>");
+        tdf.text(sv.frequency);
+        $(tr).append(tdf);
+
+        //moment stuff
+        let freqData = sv.frequency
+        
+        let firstTrainTime = 0;
+        
+        let firstTimeConv = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+        
+        let timeDif = moment().diff(moment(firstTimeConv), "minutes");
+        
+        let timeRemainder = timeDif % freqData;
+        
+        let timeToTrain = freqData - timeRemainder;
+        
+        let nxtTrn = moment().add(timeToTrain, "minutes");
+        
+        let tdna = $("<td>");
+        tdna.text(moment(nxtTrn).format('LT'));
+        $(tr).append(tdna);
+        
+        let tdtt = $("<td>");
+        tdtt.text(timeToTrain);
+        $(tr).append(tdtt);
+    });
+
+    
+
